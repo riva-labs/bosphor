5949fb7b35258de9bdabc9ebb4d98dcab7b3e1fd
AliErcanOzgokce <aliercanozgokce@gmail.com>
2026-06-09 18:54:37 +0200

    refactor(relayer): extract SuiLzService from SuiService
    Move quoteLzFee and lzSendProof into a dedicated SuiLzService that
    accesses SuiService config via lazy getters. Make signAndExecute
    public. Export LzInfra interface and add getLzConfigId, getLzOappId,
    getLzMessagingChannel, getLzInfra getters on SuiService.

--- relayer/src/chain/sui/index.ts
+++ relayer/src/chain/sui/index.ts
@@ -1,4 +1,5 @@
 export { SuiModule } from './sui.module';
 export { SuiService } from './sui.service';
 export { SuiCheckpointService } from './sui-checkpoint.service';
-export type { SuiLzEvent } from './sui.service';
+export { SuiLzService } from './sui-lz.service';
+export type { SuiLzEvent, LzInfra } from './sui.service';
--- relayer/src/chain/sui/sui.module.ts
+++ relayer/src/chain/sui/sui.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SuiService } from './sui.service';
 import { SuiCheckpointService } from './sui-checkpoint.service';
+import { SuiLzService } from './sui-lz.service';
 
 @Module({
-  providers: [SuiService, SuiCheckpointService],
-  exports: [SuiService, SuiCheckpointService],
+  providers: [SuiService, SuiCheckpointService, SuiLzService],
+  exports: [SuiService, SuiCheckpointService, SuiLzService],
 })
 export class SuiModule {}
